273474f12314a5034da8f92a7b5fa9e3292560cd
Jose Mendez <jmendez7221@gmail.com>
2025-09-29 17:26:37 -0400

    proper styling for the final task
    Added docstrings and removed spaces
    to get a 10/10 pylint score.

--- final_project/server.py
+++ final_project/server.py
@@ -1,3 +1,9 @@
+"""
+Server file to run the flask app.
+This file contains the routes and functions to handle requests and responses.
+It uses the emotion_detector function from the emotion_detection module to get the emotions from the text.
+"""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -6,7 +12,12 @@ app = Flask("Emotion detector")
 
 @app.route("/emotionDetector")
 def emotion_detect():
+    """
+    Function to detect the emotion from the given text using Watson NLP API.
     
+    Returns:
+    str: The response from the API.
+    """
     text_to_analyse = request.args.get('textToAnalyze')
 
     response = emotion_detector(text_to_analyse)
@@ -23,6 +34,7 @@ def emotion_detect():
 
 @app.route("/")
 def render_index_page():
+    """Rendering page."""
     return render_template('index.html')
 
 if __name__ == "__main__":

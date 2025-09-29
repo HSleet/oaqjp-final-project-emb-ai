1a92b9cbd7af6566ccff5e1ba9566522ce83d2e3
Jose Mendez <jmendez7221@gmail.com>
2025-09-29 15:55:40 -0400

    Initial commit
    First commit for a final project of the course "Developing AI Applications with Python and Flask" by IB

---
+++ final_project/EmotionDetection/emotion_detection.py
@@ -0,0 +1,30 @@
+import requests  
+import json
+
+def emotion_detector(text_to_analyse):
+    # Set up required params for the emotion detector
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    # Get thte response from the NLP library
+    response = requests.post(url, json = myobj, headers=header)
+
+    # Format the response into a json
+    formatted_response = json.loads(response.text)
+
+    # Extract the emotions from the response
+    emotions_dict = formatted_response['emotionPredictions'][0]['emotion']
+
+    # Find the highest scored emotion
+    dominant_emotion = ''
+    max_val = 0
+    for key,value in emotions_dict.items():
+        if value > max_val:
+            max_val = value
+            dominant_emotion = key
+    
+    # Add the dominant emotion to the response dictionary
+    emotions_dict['dominant_emotion'] = dominant_emotion
+
+    return emotions_dict
